36d74a7546d32fe060503bc9914de290c7f5625a
atGit2021 <a.c.turner.gm@gmail.com>
2021-09-29 19:03:43 -0400

    Created login endpoint, created template for user and farm registration routes and controllers, changed port number to 5000 in www/bin, created a config folder and config.env file to hold MongoDB connections string

---
+++ vegt-backend/controllers/loginHandler.js
@@ -0,0 +1,44 @@
+const User = require('../models/user');
+
+const loginHandler = (req, res, next) => {
+
+    let newUser = new User({
+        userName: req.body.userName,
+        password: req.body.password
+    });
+
+    try {
+        User.findOne({ "userName": newUser.userName, "password": newUser.password })
+            .exec(function (err, user) {
+                if (!user) {
+                    console.log("User not found");
+                    console.log("err: " + err);
+                    return res.status(404).json({ message: "Invalid User or Password" });
+                }
+                if (user.isDeleted) {
+                    console.log("User account deleted, please talk to your administrator");
+                    return res.status(403).json({ message: "User account deleted" });
+                }
+
+                console.log(user.userName + " was found!");
+
+                //Add Auth/JWT token code here in next sprint
+                // let passwordMatch = authService.comparePasswords(newUser.password, user.password);
+                // if (passwordMatch) {
+                //     let token = authService.signUser(user); // <--- Uses the authService to create jwt token
+                //     res.cookie('jwt', token); // <--- Adds token to response as a cookie
+                // } else {
+                //     console.log('Wrong password');
+                
+                // }
+
+                return res.status(200).json({ message: "User Found" });
+                
+            });
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+module.exports = loginHandler;
--- vegt-backend/routes/users.js
+++ vegt-backend/routes/users.js
@@ -1,25 +1,41 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
 
+
+//import controllers
+const loginHandler = require('../controllers/loginHandler');
+const userRegHandler = require('../controllers/userRegHandler');
+const farmRegHandler = require('../controllers/farmRegHandler');
+
+
+
+/*------------------------------------
+          ROUTE ENDPOINTS 
+--------------------------------------*/
+//route for login -> /login
+router.post('/login', async (req, res, next) => {
+
+    //call login handler 
+    loginHandler(req, res, next);
+
+});
 
 
 // route for user registration (Add User) -> /register
 router.post('/register', async (req, res, next) => {
-    
+
+    //call user registration handler 
+    userRegHandler(req, res, next);
+
 });
 
 
-//route for login -> /login
-router.post('/login', async (req, res, next) => {
-    
-});
+//route to add farm registration profile info -> /farmRegister
+router.post('/farmRegister', async (req, res, next) => {
 
+    //call farm registration handler 
+    farmRegHandler(req, res, next);
 
-
-//route to get user profile info -> /profile
-router.get('/profile', async (req, res, next) => {
-    
 });
 
 
